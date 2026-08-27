ac34aea43b8f36cadcb372e7ab06b437b86136b7
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-08-27 12:24:25 -0300

    add - restaurante

--- public/cliente/add_cliente.php
+++ public/cliente/add_cliente.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     $endereco = $_POST['endereco'];
+    $categoria = $_POST['categoria'];
 
     $sql = "INSERT INTO cliente (nome, email, telefone, endereco) VALUES ('$nome', '$email', '$telefone', '$endereco')";
 
